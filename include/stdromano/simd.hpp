fc21412a103abb5b724ad2b60b601f44850f9cbb
romainaugier <augier.romain@sfr.fr>
2025-07-31 15:16:16 +0200

    Add: utilities for uint -> fp in simd

--- include/stdromano/simd.hpp
+++ include/stdromano/simd.hpp
@@ -9,6 +9,7 @@
 
 #include "stdromano/stdromano.hpp"
 
+#include <limits>
 #include <immintrin.h>
 
 STDROMANO_NAMESPACE_BEGIN
@@ -51,6 +52,32 @@ STDROMANO_FORCE_INLINE __m256 _mm256_abs_ps(const __m256& x) noexcept
 {
     static const __m256 sign_mask = _mm256_set1_ps(-0.0f);
     return _mm256_andnot_ps(sign_mask, x);
+}
+
+STDROMANO_FORCE_INLINE __m128 _mm_cvtepu32_ps(const __m128i& x) noexcept
+{
+    static const __m128i sign_mask = _mm_set1_epi32(0x80000000);
+    static const __m128 imax = _mm_set1_ps(static_cast<float>(std::numeric_limits<std::int32_t>::max()));
+
+    const __m128i lower = _mm_andnot_si128(sign_mask, x);
+    const __m128 f = _mm_cvtepi32_ps(lower);
+    const __m128i sign_bits = _mm_and_si128(x, sign_mask);
+    const __m128 add = _mm_castsi128_ps(_mm_cmpeq_epi32(sign_bits, sign_mask));
+
+    return _mm_add_ps(f, _mm_and_ps(imax, add));
+}
+
+STDROMANO_FORCE_INLINE __m256 _mm256_cvtepu32_ps(const __m256i& x) noexcept
+{
+    static const __m256i sign_mask = _mm256_set1_epi32(0x80000000);
+    static const __m256 imax = _mm256_set1_ps(static_cast<float>(std::numeric_limits<std::int32_t>::max()));
+
+    const __m256i lower = _mm256_andnot_si256(sign_mask, x);
+    const __m256 f = _mm256_cvtepi32_ps(lower);
+    const __m256i sign_bits = _mm256_and_si256(x, sign_mask);
+    const __m256 add = _mm256_castsi256_ps(_mm256_cmpeq_epi32(sign_bits, sign_mask));
+
+    return _mm256_add_ps(f, _mm256_and_ps(imax, add));
 }
 
 /* Horizontal sums (sum the entire vector to a single element) */
