5a9907a45fc960719caecbd2fa7047a7dabeb856
romainaugier <augier.romain@sfr.fr>
2025-08-14 18:14:44 +0200

    Fix: CMake adjustments

--- cmake/target_options.cmake
+++ cmake/target_options.cmake
@@ -3,16 +3,15 @@
 # All rights reserved. 
 
 function(set_target_options target_name)
-    if(CMAKE_C_COMPILER_ID STREQUAL "Clang")
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
         set(ROMANO_CLANG 1)
-        set(CMAKE_C_FLAGS "-Wall -pedantic-errors")
         set(CMAKE_CXX_FLAGS "-Wall -pedantic-errors")
 
         target_compile_options(${target_name} PRIVATE $<$<CONFIG:Debug,RelWithDebInfo>:-fsanitize=leak -fsanitize=address>)
         target_compile_options(${target_name} PRIVATE $<$<CONFIG:Release,RelWithDebInfo>:-O3 -mavx2 -mfma)
 
         target_link_options(${target_name} PRIVATE $<$<CONFIG:Debug,RelWithDebInfo>:-fsanitize=address>)
-    elseif (CMAKE_C_COMPILER_ID STREQUAL "GNU")
+    elseif (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
         set(ROMANO_GCC 1)
         set(CMAKE_C_FLAGS "-D_FORTIFY_SOURCES=2 -pipe -Wall -pedantic-errors")
         set(CMAKE_CXX_FLAGS "-D_FORTIFY_SOURCES=2 -pipe -Wall -pedantic-errors")
@@ -39,10 +38,10 @@ function(set_target_options target_name)
 
         set(COMPILE_OPTIONS -D_FORTIFY_SOURCES=2 -pipe -Wall -pedantic-errors $<$<CONFIG:Release,RelWithDebInfo>:-O3 -ftree-vectorizer-verbose=2> -mveclibabi=svml -mavx2 -mfma)
 
-        target_compile_options(${target_name} PRIVATE $<$<COMPILE_LANGUAGE:CXX>:${COMPILE_OPTIONS}>)
-    elseif (CMAKE_C_COMPILER_ID STREQUAL "Intel")
+        target_compile_options(${target_name} PRIVATE ${COMPILE_OPTIONS})
+    elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
         set(ROMANO_INTEL 1)
-    elseif (CMAKE_C_COMPILER_ID STREQUAL "MSVC")
+    elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
         set(ROMANO_MSVC 1)
         include(find_avx)
 
@@ -56,7 +55,7 @@ function(set_target_options target_name)
         # 4146 is " unary minus operator applied to unsigned type", again we don't care (it appears only in lsb_u64)
         set(COMPILE_OPTIONS /W4 /wd4710 /wd5045 /wd4324 /wd4146 /utf-8 ${AVX_FLAGS} $<$<CONFIG:Release,RelWithDebInfo>:/O2 /GF /Ot /Oy /GT /GL /Oi /Zi /Gm- /Zc:inline /Qpar>)
 
-        target_compile_options(${target_name} PRIVATE $<$<COMPILE_LANGUAGE:CXX>:${COMPILE_OPTIONS}>)
+        target_compile_options(${target_name} PRIVATE ${COMPILE_OPTIONS})
 
         # 4300 is "ignoring '/INCREMENTAL' because input module contains ASAN metadata", and we do not care
         target_link_options(${target_name} PRIVATE /ignore:4300 /NODEFAULTLIB:library)
