0808a1ea747f281d255d45c3c6683bb3e4f00bd5
romainaugier <augier.romain@sfr.fr>
2025-09-26 11:44:00 +0200

    Fixed: text alignment in asm files

--- src/asm/x64_windows/strcmp.asm
+++ src/asm/x64_windows/strcmp.asm
@@ -84,12 +84,12 @@ asm__detail_strcmp_avx_cs PROC
 ; rcx = lhs, rdx = rhs, r8 = length
 
     ; simd_loop_size = length & ~31
-    mov     r10, r8
-    and     r10, -32                 ; r10 = simd_loop_size
-    xor     r9d, r9d                 ; r9 = i
+    mov r10, r8
+    and r10, -32 ; r10 = simd_loop_size
+    xor r9d, r9d ; r9 = i
 
-    test    r10, r10
-    je      _tail
+    test r10, r10
+    je _tail
 
 _simd:
     prefetcht0 byte ptr [rcx + r9 + 192]
@@ -97,37 +97,37 @@ _simd:
 
     vmovdqa ymm0, ymmword ptr [rcx + r9]
     vmovdqa ymm1, ymmword ptr [rdx + r9]
-    vpcmpeqb ymm2, ymm0, ymm1            ; 0xFF where equal
-    vpmovmskb eax, ymm2                  ; 32-bit mask; 1 bit per byte
-    cmp     eax, 0FFFFFFFFh              ; all equal ?
-    jne     _mismatch
+    vpcmpeqb ymm2, ymm0, ymm1 ; 0xFF where equal
+    vpmovmskb eax, ymm2 ; 32-bit mask; 1 bit per byte
+    cmp eax, 0FFFFFFFFh ; all equal ?
+    jne _mismatch
 
-    add     r9, 32
-    cmp     r9, r10
-    jb      _simd
+    add r9, 32
+    cmp r9, r10
+    jb _simd
 
 _tail:
     ; remaining_size
-    mov     r11, r8
-    sub     r11, r10                     ; r11 = remaining
-    jz      _equal
+    mov r11, r8
+    sub r11, r10 ; r11 = remaining
+    jz _equal
 
 _tail_loop:
-    mov     al,  byte ptr [rcx + r10]
-    cmp     al,  byte ptr [rdx + r10]
-    jne     _mismatch
-    inc     r10
-    dec     r11
-    jnz     _tail_loop
+    mov al,  byte ptr [rcx + r10]
+    cmp al,  byte ptr [rdx + r10]
+    jne _mismatch
+    inc r10
+    dec r11
+    jnz _tail_loop
 
 _equal:
     vzeroupper
-    mov     eax, 1
+    mov eax, 1
     ret
 
 _mismatch:
     vzeroupper
-    xor     eax, eax
+    xor eax, eax
     ret
 
 asm__detail_strcmp_avx_cs ENDP
