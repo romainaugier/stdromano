b75b4cdbd8d7fa2fc51978516ec294d68455ffbe
romainaugier <augier.romain@sfr.fr>
2026-09-24 21:47:26 +0200

    Removed HAS_BUILTIN macro as it's undefined in Clang

--- include/stdromano/maths.hpp
+++ include/stdromano/maths.hpp
@@ -524,7 +524,7 @@ STDROMANO_FORCE_INLINE void sincos(T theta, T* s, T* c) noexcept;
 template<>
 STDROMANO_FORCE_INLINE void sincos(float theta, float* s, float* c) noexcept
 {
-#if STDROMANO_HAS_BUILTIN(__builtin_sincosf)
+#if defined(__has_builtin) && __has_builtin(__builtin_sincosf)
     __builtin_sincosf(theta, s, c);
 #else
     *s = sin(theta);
@@ -535,7 +535,7 @@ STDROMANO_FORCE_INLINE void sincos(float theta, float* s, float* c) noexcept
 template<>
 STDROMANO_FORCE_INLINE void sincos(double theta, double* s, double* c) noexcept
 {
-#if STDROMANO_HAS_BUILTIN(__builtin_sincos)
+#if defined(__has_builtin) && __has_builtin(__builtin_sincos)
     __builtin_sincos(theta, s, c);
 #else
     *s = sin(theta);
