f9cf32aba33e1d60f2d9005be78dd0d138369e60
romainaugier <augier.romain@sfr.fr>
2026-09-24 21:34:40 +0200

    Fixed double test in test_json

--- tests/test_json.cpp
+++ tests/test_json.cpp
@@ -260,26 +260,6 @@ STDROMANO_TEST_CASE(parses_strings_with_escapes)
 {
     Json json;
 
-    STDROMANO_REQUIRE(parse(json, R"("a\"b\\c\/d\n\t")"));
-    STDROMANO_REQUIRE(json.root()->is_str());
-    STDROMANO_CHECK_EQ(string_of(json.root()), "a\"b\\c/d\n\t");
-
-    STDROMANO_REQUIRE(parse(json, R"("caf\u00e9 \u20ac")"));
-    STDROMANO_CHECK_EQ(string_of(json.root()), "caf\xC3\xA9 \xE2\x82\xAC");
-
-    STDROMANO_REQUIRE(parse(json, R"("\ud83d\ude00 \u0001")"));
-    STDROMANO_CHECK_EQ(string_of(json.root()), "\xF0\x9F\x98\x80 \x01");
-
-    const StringD dumped = json.dumps();
-
-    Json reparsed;
-    STDROMANO_REQUIRE(reparsed.loads(dumped.c_str(), dumped.size()));
-    STDROMANO_CHECK_EQ(string_of(reparsed.root()), "\xF0\x9F\x98\x80 \x01");
-}
-STDROMANO_TEST_CASE(parses_strings_with_escapes)
-{
-    Json json;
-
     STDROMANO_REQUIRE(parse(json, "\"a\\\"b\\\\c\\/d\\n\\t\""));
     STDROMANO_REQUIRE(json.root()->is_str());
     STDROMANO_CHECK_EQ(string_of(json.root()), "a\"b\\c/d\n\t");
